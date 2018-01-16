731905b2988d606d2f50495f7c2e8dccd9a13cdc
Jancik, Radek <radek.jancik@storaenso.com>
2018-01-16 01:20:47 +0100

    insert logger files

---
+++ shared/Log/LoggerMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace sunamo
+{
+    public class LogMessage : LogMessageAbstract<Color, string>
+    {
+
+
+        public Task<LogMessageAbstract<Color, string>> Initialize(DateTime now, TypeOfMessage st, string status, Color color)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
