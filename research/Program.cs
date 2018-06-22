06fd03f80f7ebf31e66b22b80a279618b3938843
Radek Jančík <sunamocz@outlook.com>
2018-06-23 00:46:05 +0200

    create research project

---
+++ research/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace research
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
