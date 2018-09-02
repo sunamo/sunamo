e2cf039371af1c7795662cbf33d523ef2781ab6a
Radek Jančík <sunamocz@outlook.com>
2018-09-02 13:31:00 +0200

    put life again to HostingManager

---
+++ SunamoFtp/Other/FtpLogging.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunamoFtp.Other
+{
+    public static class FtpLogging
+    {
+        public static bool GoToFolder = true;
+        public static bool GoToUpFolder = true;
+        //public static bool GoToFolder = true;
+
+    }
+}
