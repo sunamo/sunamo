234f9f3eaa35382e6237aecbc326e0199d2ac36b
Radek Jančík <radek.jancik@sunamo.cz>
2018-10-06 23:40:06 -0700

    Ads and RepairPhones uris

--- sunamo/Web/UriWebServices.cs
+++ sunamo/Web/UriWebServices.cs
@@ -5,29 +5,71 @@ using System.Text;
 
 public static class UriWebServices
 {
-    public static class Ads
+    public static class EnglishMobileParts
+    {
+        /// <summary>
+        /// eb
+        /// </summary>
+        public const string ebay = "https://www.ebay.com/sch/i.html?_nkw=%s";
+        /// <summary>
+        /// wt
+        /// </summary>
+        public const string witrigs = "https://www.witrigs.com/searchautocomplete/autoresult?q=%s";
+        /// <summary>
+        /// ae
+        /// </summary>
+        public const string aliexpress = "https://www.aliexpress.com/wholesale?SearchText=%s";
+
+        public static readonly string[] All = new string[] { ebay, witrigs, aliexpress };
+
+        public static void SearchInAll(string what)
+        {
+            UriWebServices.SearchInAll(All, what);
+        }
+    }
+
+    public static class AdsMsRegion
     {
         public const string bazosCz = "https://www.bazos.cz/search.php?hledat=%s&rubriky=www&hlokalita=70800&humkreis=25&cenaod=&cenado=&Submit=Hledat&kitx=ano";
         public const string hyperinzerceCz = "http://moravskoslezsky-kraj.hyperinzerce.cz/%s/";
         public const string bazarCz = "https://www.bazar.cz/?search=1&ft=%s&p=70800&a=25&pid=6934";
         public const string sBazarCz = "https://www.sbazar.cz/hledej/%s";
         public const string avizoCz = "https://www.avizo.cz/fulltext/?beng=1&searchfor=ads&keywords=%s";
-        public const string letGoCz = "https://www.letgo.cz/items/q-%s";
-
-        
+        public const string letGoCz = "https://www.letgo.cz/moravskoslezsky-kraj_g200003339573/q-%s";
+        public const string letGoCzPoruba = "https://www.letgo.cz/poruba_g50000007359/q-%s";
 
         public static readonly string[] All = new string[] { bazosCz, hyperinzerceCz, bazarCz, sBazarCz, avizoCz, letGoCz };
 
+        public static void SearchInAll(string what)
+        {
+            UriWebServices.SearchInAll(All, what);
+        }
+
+        /// <summary>
+        /// 70800 v okoli 25km
+        /// </summary>
+        /// <param name="what"></param>
+        /// <returns></returns>
         public static string BazosCz(string what)
         {
             return FromChromeReplacement(bazosCz, what);
         }
 
+        /// <summary>
+        /// MS kraj
+        /// </summary>
+        /// <param name="what"></param>
+        /// <returns></returns>
         public static string HyperInzerceCz(string what)
         {
             return FromChromeReplacement(hyperinzerceCz, what);
         }
 
+        /// <summary>
+        /// 70800 +25km
+        /// </summary>
+        /// <param name="what"></param>
+        /// <returns></returns>
         public static string BazarCz(string what)
         {
             return FromChromeReplacement(bazarCz, what);
@@ -51,14 +93,80 @@ public static class UriWebServices
 
     }
 
-    
-
-    public static class AdsPoruba
+    public static class AdsWholeCR
     {
-        public const string letGoCz = "https://www.letgo.cz/poruba_g50000007359/q-%s";
+        public const string bazosCz = "https://www.bazos.cz/search.php?hledat=%s&rubriky=www&cenaod=&cenado=&Submit=Hledat&kitx=ano";
+        public const string hyperinzerceCz = "https://inzeraty.hyperinzerce.cz/%s/";
+        public const string bazarCz = "https://www.bazar.cz/?search=1&ft=%s&pid=6934";
+        public const string sBazarCz = "https://www.sbazar.cz/hledej/%s";
+        public const string avizoCz = "https://www.avizo.cz/fulltext/?beng=1&searchfor=ads&keywords=%s";
+        public const string letGoCz = "https://www.letgo.cz/items/q-%s";
+        
+        public static readonly string[] All = new string[] { bazosCz, hyperinzerceCz, bazarCz, sBazarCz, avizoCz, letGoCz };
+
+        public static void SearchInAll(string what)
+        {
+            UriWebServices.SearchInAll(All, what);
+        }
+
+        /// <summary>
+        /// 70800 v okoli 25km
+        /// </summary>
+        /// <param name="what"></param>
+        /// <returns></returns>
+        public static string BazosCz(string what)
+        {
+            return FromChromeReplacement(bazosCz, what);
+        }
+
+        /// <summary>
+        /// MS kraj
+        /// </summary>
+        /// <param name="what"></param>
+        /// <returns></returns>
+        public static string HyperInzerceCz(string what)
+        {
+            return FromChromeReplacement(hyperinzerceCz, what);
+        }
+
+        /// <summary>
+        /// 70800 +25km
+        /// </summary>
+        /// <param name="what"></param>
+        /// <returns></returns>
+        public static string BazarCz(string what)
+        {
+            return FromChromeReplacement(bazarCz, what);
+        }
+
+        public static string SBazarCz(string what)
+        {
+            return FromChromeReplacement(sBazarCz, what);
+        }
+
+        public static string AvizoCz(string what)
+        {
+            return FromChromeReplacement(avizoCz, what);
+        }
+
+        public static string LetGoCz(string what)
+        {
+            return FromChromeReplacement(letGoCz, what);
+        }
+
+
     }
 
     public const string chromeSearchstringReplacement = "%s";
+
+    public static void SearchInAll(string[] array, string what)
+    {
+        foreach (var item in array)
+        {
+            string uri = UriWebServices.FromChromeReplacement(item, what);
+            Process.Start(uri);
+        }
+    }
 
     public static string FacebookProfile(string nick)
     {
