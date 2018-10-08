5b8db976ee287bdde623b71bd826a8a6e51ea673
Radek Jančík <radek.jancik@sunamo.cz>
2018-10-07 22:28:10 -0700

    Add SunamoYaml project

---
+++ SunamoYaml/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using YamlDotNet.RepresentationModel;
+
+namespace SunamoYaml
+{
+    public class Yaml
+    {
+        //YamlDotNet.Serialization.
+        YamlStream ys;
+        //Yaml
+    }
+}
