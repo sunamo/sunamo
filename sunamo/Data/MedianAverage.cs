8e61d6b0b80c216229d067b95d7b2af9f8d32626
Radek Jančík <radek.jancik@sunamo.cz>
2018-11-02 23:12:21 +0100

    t0006 MedianAverage to find shop with best prices

---
+++ sunamo/Data/MedianAverage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sunamo.Data
+{
+    public class MedianAverage<T>
+    {
+        public T median;
+        public T average;
+        public T min;
+        public T max;
+
+        public override string ToString()
+        {
+            return $"Median: {median}, Average: {average}, Min: {min}, Max: {max}";
+        }
+    }
+}
