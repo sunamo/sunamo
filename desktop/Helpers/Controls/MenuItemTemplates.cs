941e461d8785977668210dcc91f723b1c46ab10b
Radek Jančík <radek.jancik@sunamo.cz>
2018-11-02 22:54:33 +0100

    t0030 MenuItems, rename args

---
+++ desktop/Helpers/Controls/MenuItemTemplates.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace desktop.Helpers.Controls
+{
+    public class MenuItemTemplates
+    {
+        public static MenuItem AvailableShortcut(Dictionary<string, string> dictionary2)
+        {
+            MenuItem miShowControls = new MenuItem();
+            miShowControls.Click += delegate
+            {
+                WindowWithUserControl.AvailableShortcut(dictionary2);
+            };
+            miShowControls.Header = "Available shortcuts...";
+            return miShowControls;
+        }
+    }
+}
--- desktop/Helpers/Controls/ToolbarHelper.cs
+++ desktop/Helpers/Controls/ToolbarHelper.cs
@@ -4,12 +4,12 @@ using System.Windows.Input;
 using System.Windows.Media;
 public static class ToolbarHelper
 {
-    public static void AddButton(ToolBar tsAkce, ImageSource imageOnButton, string p, CommandBinding commandBinding, int whImage, int whButton)
+    public static void AddButton(ToolBar tsAkce, ImageSource imageOnButton, string tooltip, CommandBinding commandBinding, int whImage, int whButton)
     {
         Button button = new Button();
         button.Width = whButton;
         button.Height = whButton;
-        button.ToolTip = p;
+        button.ToolTip = tooltip;
         Image image = new Image(); //ImageHelper.ReturnImage(UnknownCacheImage);
         image.Source = imageOnButton;
         image.Width = whImage;
@@ -19,12 +19,12 @@ public static class ToolbarHelper
         tsAkce.Items.Add(button);
     }
 
-    public static void AddButton(ToolBar tsAkce, ImageSource imageOnButton, string p, RoutedEventHandler handler, int whImage, int whButton)
+    public static void AddButton(ToolBar tsAkce, ImageSource imageOnButton, string tooltip, RoutedEventHandler handler, int whImage, int whButton)
     {
         Button button = new Button();
         button.Width = whButton;
         button.Height = whButton;
-        button.ToolTip = p;
+        button.ToolTip = tooltip;
         Image image = new Image();  //ImageHelper.ReturnImage(UnknownCacheImage);
         image.Source = imageOnButton;
         image.Width = whImage;
