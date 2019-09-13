511380f75dc60a32ca9ab683c84eb33107949624
Radek Jančík <radek.jancik@sunamo.cz>
2019-09-13 14:13:15 +0200

    Local 13-9-19

---
+++ shared/Data/PushSolutionsData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class PushSolutionsData
+{
+    public bool mergeAndFetch = false; 
+    public bool addGitignore = false;
+
+    public void Set(bool mergeAndFetch, bool addGitignore = false)
+    {
+        this.mergeAndFetch = mergeAndFetch;
+        this.addGitignore = addGitignore;
+    }
+}
+
---
+++ shared/Helpers/GitHelper.cs
@@ -0,0 +1,105 @@
+﻿using sunamo.Generators;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class GitHelper
+{
+    public static void PushSolution(bool release, GitBashBuilder gitBashBuilder, string pushArgs, string commitMessage, string fullPathFolder, PushSolutionsData pushSolutionsData)
+    {
+        // 1. better solution is commented only getting files
+        int countFiles = 0;
+        if (release)
+        {
+            countFiles = FS.GetFiles(fullPathFolder, FS.MascFromExtension(), SearchOption.AllDirectories).Count;
+        }
+
+        if (countFiles > 0)
+        {
+            //gitStatus.Clear();
+            //gitStatus.Cd(fullPathFolder);
+            //gitStatus.Status();
+
+            var result = new List<List<string>>(CA.ToList<List<string>>(CA.ToListString(), CA.ToListString()));
+            // 2. or powershell
+            //if (release)
+            //{
+            //    result = PowershellRunner.Invoke(gitStatus.Commands);
+            //}
+
+
+            var statusOutput = result[1];
+            // If solution has changes
+            var hasChanges = CA.ReturnWhichContains(statusOutput, "nothing to commit").Count == 0;
+            if (!hasChanges)
+            {
+                foreach (var lineStatus in statusOutput)
+                {
+                    string statusLine = lineStatus.Trim();
+                    if (statusOutput.Contains("modified:"))
+                    {
+                        if (statusOutput.Contains(".gitignore"))
+                        {
+                            hasChanges = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (!hasChanges)
+            {
+                foreach (var lineStatus in statusOutput)
+                {
+                    //
+                    string statusLine = lineStatus.Trim();
+                    if (statusOutput.Contains("but the upstream is gone"))
+                    {
+                        hasChanges = true;
+                        break;
+                    }
+                }
+
+            }
+
+            // or/and is a git repository
+            var isGitRepository = CA.ReturnWhichContains(statusOutput, "not a git repository").Count == 0;
+            if (hasChanges && isGitRepository)
+            {
+                gitBashBuilder.Cd(fullPathFolder);
+
+                if (pushSolutionsData.mergeAndFetch)
+                {
+                    gitBashBuilder.Fetch();
+                }
+
+                gitBashBuilder.Add(AllStrings.asterisk);
+
+                gitBashBuilder.Commit(false, commitMessage);
+
+                if (pushSolutionsData.mergeAndFetch)
+                {
+                    gitBashBuilder.Merge("--allow-unrelated-histories");
+                }
+
+                if (pushSolutionsData.addGitignore)
+                {
+                    gitBashBuilder.Add(".gitignore");
+                }
+
+                gitBashBuilder.Push(pushArgs);
+
+                gitBashBuilder.AppendLine();
+
+                // Dont run, better is paste into powershell due to checking errors
+                //var git = gitBashBuilder.Commands;
+                //PowershellRunner.Invoke(git);
+            }
+        }
+    }
+}
+
