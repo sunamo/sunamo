5bef43b980c5dcba852f8ee0a095858e21b122aa
Radek Jančík <sunamocz@outlook.com>
2019-11-07 23:35:04 +0100

    From VPS

---
+++ sunamo/VpsHelperSunamo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class VpsHelperSunamo
+{
+    public const string path = @"c:\_";
+
+    public static bool IsVps
+    {
+        get
+        {
+            return FS.ExistsDirectory(path);
+        }
+    }
+
+    public static string SunamoSln()
+    {
+        if (IsVps)
+        {
+            return @"c:\_\sunamo\";
+        }
+        else
+        {
+            return @"d:\Documents\Visual Studio 2017\Projects\sunamo\";
+        }
+    }
+
+    public static string SunamoProject()
+    {
+        return FS.Combine(SunamoSln(), "sunamo");
+    }
+}
+
