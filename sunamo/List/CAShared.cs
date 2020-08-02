677db65c52f91182c4370bc8760cdeaad62699f3
Radek Jančík <radek.jancik@sunamo.cz>
2020-08-02 14:56:08 +0200

    Mass commit 2-8-2020

--- sunamo/List/CAShared.cs
+++ sunamo/List/CAShared.cs
@@ -13,7 +13,32 @@ using System.Text.RegularExpressions;
 
 public static partial class CA
 {
+    public static int Count(IEnumerable e)
+    {
+        if (e == null)
+        {
+            return 0;
+        }
 
+        if (e is IList)
+        {
+            return (e as IList).Count;
+        }
+
+        if (e is Array)
+        {
+            return (e as Array).Length;
+        }
+
+        int count = 0;
+
+        foreach (var item in e)
+        {
+            count++;
+        }
+
+        return count;
+    }
 
     private static Type type = typeof(CA);
 
